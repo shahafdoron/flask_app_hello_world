c5a80d831b1a8fd1190ac8a62d8aa3621b33b89b
shahafdoron <shahaf1doron@gmail.com>
2021-08-30 16:02:48 +0300

    flask first commit

---
+++ my_flask_app/app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class HomePage(Resource):
+    '''
+    To get the home page context
+    '''
+
+    def get(self):
+
+        response_body={
+            "message": "message content",
+        }
+
+        app.logger.info("response body : {}".format(response_body))
+
+        return response_body , 200 if None not in response_body.values() else 400
+
+api.add_resource(HomePage,"/")
+
+if __name__ == '__main__':
+    app.run(port=5000,debug=True)
+
